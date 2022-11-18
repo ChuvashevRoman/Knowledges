702cc7d99fb365481c0ce85089429027a20d7170
Roman Chuvashev <roman.chuvashev@mximumtest.ru>
2022-11-18 12:21:09 +0300

    CPP CH4

---
+++ CPP/programs/CH4/week.cpp
@@ -0,0 +1,18 @@
+// dayenum.cpp
+// применение перечислений
+#include <iostream>
+using namespace std; 
+// объявление перечисляемого типа
+enum days_of_week { Sun, Mon, Tue, Wed, Thu, Fri, Sat };
+int main()
+{
+days_of_week day1, day2;	// определения переменных, хранящих дни недели
+day1 = Mon; 				// инициализация переменных
+day2 = Thu;
+int diff = day2 - day1;		// арифметическая операция
+cout << "Разница в днях: " << diff << endl;
+if(day1 < day2)
+// сравнение
+cout << "day1 наступит раньше, чем day2\n";
+return 0;
+}
